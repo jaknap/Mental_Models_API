d376834b171e124b53e240891cd7dbd1ac7a88f5
jaknap <pankz9@gmail.com>
2018-09-24 22:05:38 -0400

    push code

---
+++ src/components/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import '../styles/App.css';
+import FetchData from "./FetchData";
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            biasSelected: 'numeracy',
+            optionSelected : 'Numeracy'
+        }
+        this.handleSelect = this.handleSelect.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+
+
+    handleSelect(event){
+        const biasDict = {
+            Numeracy:'numeracy',
+            Systems:'system',
+            Physical:'pw',
+            Biological:'bio',
+            HumanNature:'humannature',
+            MicroeconomicsStrategy:'mes',
+            MilitaryWar:'military'
+        }
+        console.log(event.target.value);
+        this.setState({ biasSelected: biasDict[event.target.value], optionSelected:event.target.value });
+    }
+
+    handleSubmit(){
+        console.log(this.state);
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                <select value={this.state.optionSelected}
+                        onChange={this.handleSelect}>
+                    <option>Numeracy</option>
+                    <option>Systems</option>
+                    <option>Physical</option>
+                    <option>Biological</option>
+                    <option>HumanNature</option>
+                    <option>MicroeconomicsStrategy</option>
+                    <option>MilitaryWar</option>
+                </select>
+                </div>
+                <FetchData selected={this.state.biasSelected}/>
+            </div>
+        );
+    }
+}
+
+
+export default App;
---
+++ src/components/FetchData.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import '../styles/App.css';
+import App from './App';
+
+class FetchData extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            loading: true,
+            biases: []
+        }
+    }
+
+
+    async componentDidMount(){
+            const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+            const API_URL =  `https://shrouded-atoll-30731.herokuapp.com/api/v1/mm/numeracy`;
+            const response = await fetch(PROXY_URL+API_URL);
+            const data = await response.json();
+            console.log(data);
+            this.setState({biases : data, loading: false})
+    
+            }
+
+    async componentWillReceiveProps(nextProps){
+        if(nextProps.selected!==this.props.selected){
+            console.log("first "+this.props.selected);
+            let selectedURL = nextProps.selected;      
+            const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+            const API_URL =  `https://shrouded-atoll-30731.herokuapp.com/api/v1/mm/${selectedURL}`;
+            const response = await fetch(PROXY_URL+API_URL);
+            const data = await response.json();
+            console.log(data);
+            this.setState({biases : data, loading: false})
+    
+            }
+        }
+    render() {
+        return (
+            <div>
+                <h1></h1>
+                {this.state.loading ? 
+                    '...loading' :
+
+                    // (<div>
+                    //     {this.state.people.map( (person) => 
+                    //         <div key={person.login.uuid}>
+                    //           <div>{person.name.title}</div>
+                    //           <div>{person.name.first}</div>
+                    //           <img src={person.picture.large}/>
+                    //         </div>
+                    //     )}
+                    // </div>
+                    // )
+                    (<div>
+                           {this.state.biases.map( (bias) => 
+                            <div key={bias._id}>
+                               <div>{bias.name}</div>
+                            </div>
+                    )}
+                    </div>
+                    )
+                }
+            </div>
+        );
+    }
+}
+
+
+export default FetchData;
